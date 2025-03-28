13ca486843ec07a8f064a2a10c4b975266f9e7dd
Sam Patel <sampatel379@gmail.com>
2025-03-28 20:31:37 +0000

    Added code to print results from checking password strength.

--- main.py
+++ main.py
@@ -8,18 +8,7 @@ import pprint as pretty_print
 print("\nPassword Strength Checker")
 password = input("\nEnter a password: ")
 result = zx.zxcvbn(password)
-pretty_print.pprint(result)
 
-
-
-
-
-# Default to code!
-
-# So use version control with git.
-# Use chatGPT to provide feedback on how to build a strong password. 
-# The more you code the more ideas you get and you search stuff up and expose yourself to new ideas
-# So you learn what's possible 
-
-# This is your phrase. 
-# Default to code!
+print(f"Score: {result["score"]}/4")
+print(f"{result['crack_times_display']["offline_slow_hashing_1e4_per_second"]}")
+print(result["feedback"]["suggestions"])
