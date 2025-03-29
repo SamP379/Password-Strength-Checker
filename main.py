32cccfac0544c9739e1eff2201d75ae132dac7bd
Sam Patel <sampatel379@gmail.com>
2025-03-29 14:22:11 +0000

    Added generate_response() function and removed generate_feedback() function.

--- main.py
+++ main.py
@@ -3,10 +3,23 @@ from langchain_ollama import OllamaLLM
 
 
 
+def get_password():
+    while not (password := input("Enter password: ")):
+        pass
+    return password
+
 
 def assess_password(password : str) -> dict:
     assessment = zx.zxcvbn(password)
     return assessment
+
+
+def display_assessment_results(assessment : dict):
+    password_score = assessment["score"]
+    password_crack_time = assessment['crack_times_display']["offline_slow_hashing_1e4_per_second"]
+    print(f"\nScore: {password_score}/4")
+    print(f"Crack time: {password_crack_time}\n")
+
 
 def generate_feedback(password : str) -> str:
     model = OllamaLLM(model = "llama3.2")
@@ -14,26 +27,26 @@ def generate_feedback(password : str) -> str:
     return feedback
 
 
+
+
+def generate_response(prompt : str) -> str:
+    model = OllamaLLM(model = "llama3.2")
+    response = model.invoke(input = prompt)
+    return response
+
+
+
 def main():
-
-    # Get password and assess it's strength
-    print("\nPassword Strength Checker")
-    password = input("\nEnter password: ")
+    print("\nPassword Strength Checker\n")
+    password = get_password()
     password_assessment = assess_password(password)
-
-    # Display password feedback
-    password_score = password_assessment["score"]
-    password_crack_time = password_assessment['crack_times_display']["offline_slow_hashing_1e4_per_second"]
-    print(f"\nScore: {password_score}/4")
-    print(f"Crack time: {password_crack_time}\n")
+    display_assessment_results(password_assessment)
+    ai_response = generate_response(f"Short response. No first person. Make a joke about how secure this password is: {password}")
+    print(ai_response + "\n")
 
 
-    wants_feedback = input("Would you like some feedback?")
 
-    #feedback = generate_feedback(password)
-    #print(feedback)
-
-
+    
 
 
 main()
