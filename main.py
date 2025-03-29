d8a8e1d326054f39e4946602ee099374e45cfb29
Sam Patel <sampatel379@gmail.com>
2025-03-29 18:29:40 +0000

    Refactored code.

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import zxcvbn as zx
 from langchain_ollama import OllamaLLM
 
 
+AI_JOKE_PROMPT = "Short response. No first person. Make a joke about how secure this password is: "
+
+
 def get_password() -> str:
     """Prompts the user to enter a password and returns it."""
     while not (password := input("Enter password: ")):
@@ -16,34 +19,38 @@ def assess_password(password : str) -> dict:
 
 
 def display_assessment_results(assessment : dict):
+    """Displays the results of the password assessment using the zxcvbn library."""
     password_score = assessment["score"]
     password_crack_time = assessment['crack_times_display']["offline_slow_hashing_1e4_per_second"]
     print(f"\nScore: {password_score}/4")
     print(f"Crack time: {password_crack_time}\n")
 
 
-def generate_response(prompt : str) -> str:
+def generate_ai_response(prompt : str) -> str:
+    """Generates a response from the llama3.2 LLM using a given prompt."""
     model = OllamaLLM(model = "llama3.2")
     response = model.invoke(input = prompt)
     return response
 
 
 def main():
+
     print("\nPassword Strength Checker\n")
+
+    # Get a password from the user and assess its strength
     password = get_password()
     password_assessment = assess_password(password)
     display_assessment_results(password_assessment)
-    ai_response = generate_response(f"Short response. No first person. Make a joke about how secure this password is: {password}")
-    print(ai_response + "\n")
 
-    wants_feedback = input("Would you like some feedback on how to make the password stronger?").lower()
-    if wants_feedback == "yes":
-        ai_response = generate_response(f"Short response. Give feedback on how to make this password stronger, you can be a bit humours based on what the password is but still educational: {password}")
-        print(ai_response)
-    
+    # Get an ai-generated joke about the entered password
+    ai_prompt = AI_JOKE_PROMPT + password
+    password_joke = generate_ai_response(prompt = ai_prompt)
+    print(password_joke + "\n")
 
-
-
+    # wants_feedback = input("Would you like some feedback on how to make the password stronger?").lower()
+    # if wants_feedback == "yes":
+    #     ai_response = generate_response(f"Short response. Give feedback on how to make this password stronger, you can be a bit humours based on what the password is but still educational: {password}")
+    #     print(ai_response)
     
 
 
